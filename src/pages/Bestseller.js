74c793cb37c58c16d1f6a99782995ae66212bdb7
Shreya-sg <82178764+Shreya-sg@users.noreply.github.com>
2021-07-13 12:17:44 +0530

    bestseller-changes

--- src/pages/Bestseller.js
+++ src/pages/Bestseller.js
@@ -3,7 +3,10 @@ import { Button } from 'reactstrap';
 
 function Bestseller(){
     return (
-        <h1>Hello</h1>
+        <>
+           <h1>Hello</h1>
+           <p>Hello world!!!Welcome...You are in Bestseller page</p>
+        </>
     )
 }
 export default Bestseller;
